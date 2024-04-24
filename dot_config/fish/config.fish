cca1a05fe055ed3bb6854dae3f14d1b018b3351c
jefo <jefo.work@gmail.com>
2024-04-24 23:41:52 +0700

    Initial commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,9 @@
+# Установка оболочки по умолчанию в fish
+set -g fish_user_shell /usr/bin/fish
+
+set --erase fish_greeting
+
+starship init fish | source
+
+
+
---
+++ dot_config/nvim/lua/configs/conform.lua
@@ -0,0 +1,18 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    css = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
+    javascript = { { "prettierd", "prettier" } },
+    typescript = { { "pretierd", "prettier" } },
+    svelte = { { "prettierd", "prettier" } }
+  },
+
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
+}
+
+require("conform").setup(options)
